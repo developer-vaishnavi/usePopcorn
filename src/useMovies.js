cd34e6c0038e1dcaa4c2a2704c2566e4f0b1eb08
Vaishnavi Sinha <mahisinha2113@gmail.com>
2024-06-15 11:05:20 +0530

    Updating

--- src/useMovies.js
+++ src/useMovies.js
@@ -14,7 +14,7 @@ export function useMovies(query, callback) {
           setLoading(true);
           setError("");
           const rest = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!rest.ok) {
